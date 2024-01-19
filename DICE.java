aaa99809c765d286800cfac116f6233da081c8fe
nuakc <146883473+nuakc@users.noreply.github.com>
2024-01-19 09:27:12 -1000

    Update and rename die.java to DICE.java

--- DICE.java
+++ DICE.java
@@ -1,4 +1,13 @@
-public static void main(String[] args) 
+/**
+ * This class is a dice rolling program that will be used to 
+ * learn new techniques and ultimately create a program that can 
+ * receive a user's input in order to roll different amounts of dice.
+ */
+import java.util.Scanner;
+public class DiceRollerProgram 
+{
+
+	public static void main(String[] args) 
 	{	
 		//create a scanner
 		Scanner rawr = new Scanner(System.in);
@@ -62,12 +71,41 @@ public static void main(String[] args)
 		int a = Integer.valueOf(left);
 		int b = Integer.valueOf(right);
 		
+		
 		//prints out numbers left and right of d
 		System.out.println("The first number is " + a + " and the second number is " + b + ".");
 		
 		//prints out sum of the two numbers
-		System.out.println("The sum of of your roll is " + (a+b) + ".");
+		//System.out.println("The sum of of your roll is " + (a+b) + ".");
 		
+		//create method RollDice
+		rollDice(a, b);
+		
+	}
+	/**
+	 * This method rolls the numbers like dice, left number = the amount of dice and
+	 * the right number = the amount of faces the dice has. For example, if the String 
+	 * is 3d6, instead of printing out "..and their sum is 9" you will now print out 
+	 * "and rolling 3 6-sided dice gives 13." (13 is not set in stone- it could be any
+	 * number you might get from rolling 3 dice.)
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	private static void rollDice(int a, int b) 
+	{
+		//for loop that uses ints a and b from separateNums method, and rolls a amount of 
+		//dice with b amount of faces
+		int llama = 0;
+		
+		for(int i = 1; i <= a; i++) 
+		{
+			//rolls b faced die
+			int beaver = ((int)(Math.random() * b +1));
+			
+			llama += beaver;
+		}
+		System.out.println("Rolling " + a + " " + b + "-faced dice gave you " + llama);
 	}
 
 	/**
