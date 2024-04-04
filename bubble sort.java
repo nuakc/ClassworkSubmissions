4c7a089735d034fe6c7de92f3018fc8f7b98ff7d
nuakc <146883473+nuakc@users.noreply.github.com>
2024-04-04 08:50:11 -1000

    Update bubble sort.java

--- bubble sort.java
+++ bubble sort.java
@@ -3,28 +3,44 @@ public class BubbleSort
 
 	public static void main(String[] args) 
 	{
-		
+		//call a method that generates an array 
+		//of random elements
 		makeRandom();
 		
-		
+		//call method that bubble sorts the generated array
+		//from least to greatest
 		bubbleSort(makeRandom());
 		
+		//call method that is able to print out the
+		//elements of the generated array
 		printArr(bubbleSort(makeRandom()));
 
 	}
 
 	/**
-	 * 
+	 * this method is able to bubble sort an array of any length
+	 * by comparing the index with the number right after it and
+	 * which of the two is greater so it can order the array properly.
 	 * @param arr
 	 * @return
 	 */
 	private static int[] bubbleSort(int[] arr) 
 	{
+		//declare int assigned the value of the length of array
 		int n = arr.length; 
+		
+		//for loop to traverse the array all the way to the last
+		//element
         for (int i = 0; i < n - 1; i++) 
-        {   for (int j = 0; j < n - i - 1; j++) 
+        {   
+        	//for loop that again traverses the array with the purpose of
+        	//comparing each element at index j to the index at j+1
+        	for (int j = 0; j < n - i - 1; j++) 
         	{
-                if (arr[j] > arr[j + 1]) 
+               //swaps the two elements should the one at index j be greater
+        	   //than the one at the index of j+1, in turn pushing all the greater
+        	   //elements towards the end and lesser ones to the front
+        		if (arr[j] > arr[j + 1]) 
                 { 
                     // swap temp and arr[i] 
                     int temp = arr[j]; 
