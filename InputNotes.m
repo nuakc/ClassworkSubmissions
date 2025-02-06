b5b39f77159c15b70d4a8f63b739889593787831
nuakc <146883473+nuakc@users.noreply.github.com>
2025-02-06 12:22:35 -0800

    Create InputNotes.m

---
+++ InputNotes.m
@@ -0,0 +1,16 @@
+clc; clear; close all
+
+m = input('Enter the start value of mass:')
+k = input('Enter the value of stiffness:')
+xi = 
+
+%%
+clc;
+x = input('Enter your name:', 's');
+disp(['Hello,', x, '!']);
+
+%%
+clc;
+y = input('Enter value for the angle in degrees:');
+z = sind(y);
+disp(['The sine value for the angle is:', num2str(z)])
